94e12715c0b69b90c264627dc8715068bc45f648
twishapatel09 <naimeshvaja4@gmail.com>
2020-05-01 13:55:00 +0530

    first commit

---
+++ precache-manifest.6441b7a29ae950359b9c9c1997081da9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6e92cd60701ef839597c17a0943736",
+    "url": "/index.html"
+  },
+  {
+    "revision": "588da46f13e7d1333fb3",
+    "url": "/static/css/main.409be89d.chunk.css"
+  },
+  {
+    "revision": "852bf9bd473ee1fe4d16",
+    "url": "/static/js/2.f146625a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.f146625a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "588da46f13e7d1333fb3",
+    "url": "/static/js/main.156e6123.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
